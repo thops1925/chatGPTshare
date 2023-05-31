3d295ed96dd57882d3a16cd914498a96be1f8f14
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-05-31 13:29:25 +0800

    save fix error todo

--- app/(client)/create-prompt/page.tsx
+++ app/(client)/create-prompt/page.tsx
@@ -1,27 +1,43 @@
 'use client';
-import Form from '@components/Form';
+
 import { useState } from 'react';
-import router from 'next/router';
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Form from '@components/Form';
 
 const CreatePrompt = () => {
-	// const { data: session } = useSession();
+	const router = useRouter();
+	const { data: session } = useSession();
 
-	const [submitting, setSubmitting] = useState(false);
-	const [post, setPost] = useState({
-		prompt: '',
-		tag: '',
-	});
+	const [submitting, setIsSubmitting] = useState(false);
+	const [post, setPost] = useState({ prompt: '', tag: '' });
 
-	const createPrompt = async (e: any) => {
+	const createPrompt = async (e) => {
 		e.preventDefault();
+		setIsSubmitting(true);
+
+		try {
+			const response = await fetch('/api/prompt/new', {
+				method: 'POST',
+				body: JSON.stringify({
+					prompt: post.prompt,
+					userId: session?.user.id,
+					tag: post.tag,
+				}),
+			});
+
+			if (response.ok) {
+				router.push('/');
+			}
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsSubmitting(false);
+		}
 	};
 
-	return (
-		<div>
-			<Form type='Create Post' post={post} setPost={setPost} submitting={submitting} handleSubmit={createPrompt} />
-		</div>
-	);
+	return <Form type='Create' post={post} setPost={setPost} submitting={submitting} handleSubmit={createPrompt} />;
 };
 
 export default CreatePrompt;
